98cbd51f7ff67df24b7254eb485642bd20ff30d5
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2025-04-15 17:16:59 +0100

    Initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "./Login";
+import Form from "./Form";
+
+function Input(props) {
+  return <input type={props.type} placeholder={props.placeholder} />;
+}
+export default Input;
