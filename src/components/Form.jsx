ad1b3453082d1f93ba34ebaa0d8192494821ce73
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2025-04-15 17:20:56 +0100

    Update Form.jsx
    switched the props.isregistered == false to !props.isregistered

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      {props.isRegistered == false && (
+      {!props.isRegistered && (
         <input type="password" placeholder="Confirm Password" />
       )}
       <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
